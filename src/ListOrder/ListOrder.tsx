ace5ae0444bd2fbd793b335bc3fe9c19a61a7ca1
fadilmuh22 <fadilmuh2002@gmail.com>
2023-05-25 16:57:28 +0700

    fix: list order remove container class

--- src/ListOrder/ListOrder.tsx
+++ src/ListOrder/ListOrder.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
+
 import Content from "./Content";
-import Footer from "./Footer";
 import Header from "./Header";
 import Status from "./Status";
 
@@ -8,11 +8,10 @@ export const ListOrder = () => {
   const [halaman, setHalaman] = useState(1);
 
   return (
-    <div className="container">
+    <div>
       <Header />
       <Status halaman={halaman} setHalaman={setHalaman} />
       <Content halaman={halaman} />
-      <Footer />
     </div>
   );
 };

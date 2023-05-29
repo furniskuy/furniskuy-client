aee159c0275856afc728bd2af872535b6ae910f9
frahari7 <frahariputra@upi.edu>
2023-05-29 14:56:56 +0700

    update list order & keranjang fix

--- src/ListProduct/ulasan.tsx
+++ src/ListProduct/ulasan.tsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import "./Ulasan.css";
+import styles from "./Ulasan.module.css";
 import PPulasan1 from "../assets/PPulasan1.png";
 import PPulasan2 from "../assets/PPulasan2.png";
 import PPulasan3 from "../assets/PPulasan3.png";
@@ -10,34 +10,34 @@ import BGulasan3 from "../assets/BGulasan3.png";
 function Ulasan() {
   return (
     <>
-      <div className="_2kontainer">
-        <div className="PenilaianProduk">
-          <div className="judulpenilaian">
+      <div className={styles["_2kontainer"]}>
+        <div className={styles["PenilaianProduk"]}>
+          <div className={styles.judulpenilaian}>
             <p>
               {" "}
-              <span className="penilaianprdk"> Penilaian Produk </span>
+              <span className={styles.penilaianprdk}> Penilaian Produk </span>
             </p>
             <p>Ulasan Customers</p>
           </div>
         </div>
-        <div className="multicardulasan">
-          <div className="cardUlasan">
-            <img src={BGulasan1} alt="" className="BGulasan" />
-            <div className="cobacoba">
-              <div className="photocard">
-                <img src={PPulasan1} alt="ah" className="PPulasan" />
+        <div className={styles.multicardulasan}>
+          <div className={styles.cardUlasan}>
+            <img src={BGulasan1} alt="" className={styles.BGulasan} />
+            <div className={styles.cobacoba}>
+              <div className={styles.photocard}>
+                <img src={PPulasan1} alt="ah" className={styles.PPulasan} />
               </div>
-              <div className="DescUlasan">
+              <div className={styles.DescUlasan}>
                 <p>
                   {" "}
-                  <span className="namauser">Fadil </span>
+                  <span className={styles.namauser}>Fadil </span>
                 </p>
                 <p>
-                  <span className="profesiuser">Pedagang Asongan</span>
+                  <span className={styles.profesiuser}>Pedagang Asongan</span>
                 </p>
                 <p>
                   {" "}
-                  <span className="reviewuser">
+                  <span className={styles.reviewuser}>
                     {" "}
                     “Terimakasih banyak, kini ruanganku menjadi lebih mewah dan
                     terlihat mahal“{" "}
@@ -47,23 +47,23 @@ function Ulasan() {
             </div>
           </div>
 
-          <div className="cardUlasan">
-            <img src={BGulasan2} alt="" className="BGulasan" />
-            <div className="cobacoba">
-              <div className="photocard">
-                <img src={PPulasan2} alt="ah" className="PPulasan" />
+          <div className={styles.cardUlasan}>
+            <img src={BGulasan2} alt="" className={styles.BGulasan} />
+            <div className={styles.cobacoba}>
+              <div className={styles.photocard}>
+                <img src={PPulasan2} alt="ah" className={styles.PPulasan} />
               </div>
-              <div className="DescUlasan">
+              <div className={styles.DescUlasan}>
                 <p>
-                  <span className="namauser">Prilly </span>
+                  <span className={styles.namauser}>Prilly </span>
                 </p>
                 <p>
                   {" "}
-                  <span className="profesiuser">Ibu Rumah Tangga</span>
+                  <span className={styles.profesiuser}>Ibu Rumah Tangga</span>
                 </p>
                 <p>
                   {" "}
-                  <span className="reviewuser">
+                  <span className={styles.reviewuser}>
                     “Makasih Furniskuy, aku sekarang berasa tinggal di apartment
                     karena barang-barang yang terlihat “{" "}
                   </span>
@@ -72,23 +72,23 @@ function Ulasan() {
             </div>
           </div>
 
-          <div className="cardUlasan">
-            <img src={BGulasan3} alt="" className="BGulasan" />
-            <div className="cobacoba">
-              <div className="photocard">
-                <img src={PPulasan3} alt="ah" className="PPulasan" />
+          <div className={styles.cardUlasan}>
+            <img src={BGulasan3} alt="" className={styles.BGulasan} />
+            <div className={styles.cobacoba}>
+              <div className={styles.photocard}>
+                <img src={PPulasan3} alt="ah" className={styles.PPulasan} />
               </div>
-              <div className="DescUlasan">
+              <div className={styles.DescUlasan}>
                 <p>
-                  <span className="namauser">Jerome</span>
+                  <span className={styles.namauser}>Jerome</span>
                 </p>
                 <p>
                   {" "}
-                  <span className="profesiuser"> Karyawan Swasta </span>
+                  <span className={styles.profesiuser}> Karyawan Swasta </span>
                 </p>
                 <p>
                   {" "}
-                  <span className="reviewuser">
+                  <span className={styles.reviewuser}>
                     “Sangat terjangkau untuk kantong saya yang tidak terlalu
                     banyak“
                   </span>

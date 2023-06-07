56352adfd9a4141abad382cb3fe15f2eb3fb62a6
fadilmuh22 <fadilmuh2002@gmail.com>
2023-06-07 21:54:33 +0700

    fix: keranjang image contain

--- src/pages/Keranjang/KeranjangItem.tsx
+++ src/pages/Keranjang/KeranjangItem.tsx
@@ -68,7 +68,7 @@ export const KeranjangItem: FunctionComponent<Props> = ({
           style={{
             width: "100%",
             height: "100%",
-            objectFit: "cover",
+            objectFit: "contain",
           }}
         />
       </td>

06c82c1a2f7fbc3da22369c4ef3377b82f377e31
fadilmuh22 <fadilmuh2002@gmail.com>
2023-06-08 12:55:30 +0700

    fix: build

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { toast } from "react-toastify";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_API_URL,
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import { Footer } from "./Footer";
